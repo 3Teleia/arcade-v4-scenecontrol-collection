095650fd076f85b2a261c00f6565a59f12a0a138
3Teleia <80172777+3Teleia@users.noreply.github.com>
2022-11-28 14:08:44 +0200

    More support for 0ms SCs and the video BG
    Better handling of commands happening at 0ms in the chart (not perfect due to technical reasons). Included support for the video background via the keyword/filename "video".

--- background_sc/triteleia_bg_sc.lua
+++ background_sc/triteleia_bg_sc.lua
@@ -4,25 +4,34 @@ bg_layer_group_channels = {}
 bg_canvases = {}
 bg_top_layer = 0
 
--- Adds an alpha channel to the base BG and saves it
+-- Replace the alpha channel of the base BG
 base = Scene.background
 bg_alpha_channels["base"] = Channel.named("base").keyframe().addKey(0,255)
-base.colorA = bg_alpha_channels["base"]
+
+-- Replace alpha, layer and sort channels of the video BG (unlike the base BG it's technically a sprite
+-- so it has its own layer and sort values that can be controlled easily
+videobg = Scene.videoBackground
+bg_alpha_channels["video"] = Channel.named("video").keyframe().addKey(0,255)
+bg_layer_channels["video"] = Channel.named("video").keyframe().setDefaultEasing('inconst').addKey(0,videobg.sort.valueAt(0))
+bg_layer_group_channels["video"] = StringChannel.create().addKey(0,videobg.layer.valueAt(0))
+videobg.colorA = bg_alpha_channels["video"]
+videobg.sort = bg_layer_channels["video"]
+videobg.layer = bg_layer_group_channels["video"]
 
 function create_bg (filename)
 	-- Check if the filename is a normal string
 	-- otherwise it crashes if you use the SC menu in Arcade and add a new line with the + button
 	if filename ~= nil and filename ~= '' and type(filename) == "string" then
 	
-		-- Initializes a new channel for the new BG's alpha values and defaults it to 0
+		-- Initialize a new channel for the new BG's alpha values and default it to 0
 		bg_alpha_channels[filename] = Channel.named(filename).keyframe().setDefaultEasing('l').addKey(0,0)
 		
-		-- Increments the value used to keep track of the topmost BG layer
+		-- Increment the value used to keep track of the topmost BG layer
 		bg_top_layer = bg_top_layer + 1
 		
-		-- Creates a new image from the given filename
+		-- Create a new image from the given filename
 		local new_bg = Scene.createImage(filename)
-		-- Sets the image's width and height respectively
+		-- Set the image's width and height respectively
 		new_bg.rectW = Channel.constant(1280)
 		new_bg.rectH = Channel.constant(960)
 		
@@ -34,55 +43,61 @@ function create_bg (filename)
 		local new_canvas = Scene.createCanvas(false)
 		
 		-- Places the new BG on the top of the BG "stack", uses a channel so that the layer and layer group can be easily changed later on
-		bg_layer_channels[filename] = Channel.named(filename).keyframe().setDefaultEasing('outconst').addKey(0,bg_top_layer)
+		bg_layer_channels[filename] = Channel.named(filename).keyframe().setDefaultEasing('inconst').addKey(0,bg_top_layer)
 		bg_layer_group_channels[filename] = StringChannel.create().addKey(0,"Background")
 		
 		new_canvas.layer = bg_layer_group_channels[filename]
 		new_canvas.sort = bg_layer_channels[filename]
 		
-		-- Makes the new canvas use the alpha values given by the previously defined channel
+		-- Make the new canvas use the alpha values given by the previously defined channel
 		new_canvas.alpha = bg_alpha_channels[filename]
 		
-		-- Saves the canvas for later usage (for bgshow and bgsetlayer)
+		-- Save the canvas for later usage in bgshow and bgsetlayer
 		bg_canvases[filename] = new_canvas
 		
-		-- Parents the new BG to the canvas so that changing the alpha or layering of the canvas would affect the BG as well
+		-- Parent the new BG to the canvas so that changing the alpha or layering of the canvas would affect the BG
 		new_bg.setParent(bg_canvases[filename])
 	end
 end
 
 function bg_change_opacity(timing, end_timing, filename, alpha, easing)
 	if filename ~= nil and filename ~= '' and type(filename) == "string" then
-		-- Gets the alpha value of the desired BG at the start timing
+		-- Get the alpha value of the specified BG at the start timing
 		local start_alpha = 0
-	
 		if filename == "base" then
 			start_alpha = base.colorA.valueAt(timing)
+		elseif filename == "video" then
+			start_alpha = videobg.colorA.valueAt(timing)
 		else
 			start_alpha = bg_canvases[filename].alpha.valueAt(timing)
 		end
-	
-		-- Adds two keys to the alpha value channel associated with the BG
-		-- The first key uses the start alpha value and is used to make sure the alpha value of the bg doesn't change
-		-- between multiple usages of bgshow on the same BG
-		-- The second key is the one that actually changes the BG's alpha to the desired value
-		bg_alpha_channels[filename]
-		.addKey(timing, start_alpha, easing)
-		.addKey(end_timing, alpha, easing)
+		
+		if timing == 0 and end_timing == 0 then
+			-- Handle bgshow usage at 0ms by removing the default 0ms 0 alpha key and replacing it
+			bg_alpha_channels[filename].removeKeyAtTiming(0)
+			bg_alpha_channels[filename].addKey(0,alpha,easing)
+		else
+			-- Add two keys to the alpha channel associated with the BG
+			-- The first key uses the start alpha value and is used to make sure the alpha value of the bg doesn't change
+			-- between multiple usages of bgshow on the same BG
+			-- The second key is the one that actually changes the BG's alpha to the specified value
+			bg_alpha_channels[filename]
+			.addKey(timing, start_alpha, easing)
+			.addKey(end_timing, alpha, easing)
+		end
 	end
 end
 
 function bg_set_layer(timing, filename, layer, layer_group)
 	if filename ~= nil and filename ~= '' and type(filename) == "string" then
-		-- Sets the layer of the canvas of the selected BG to the provided layer
-		local start_layer = bg_canvases[filename].sort.valueAt(timing)
-		bg_layer_channels[filename]
-		.addKey(timing-1, start_layer)
-		.addKey(timing, layer)
+		-- Set the layer of the canvas of the specified BG to the provided layer
+		bg_layer_channels[filename].addKey(timing, layer)
 		
 		if type(layer_group) == "string" and (string.lower(layer_group) == "foreground" or string.lower(layer_group) == "background") then
-			local start_group = bg_canvases[filename].layer.valueAt(timing)
-			bg_layer_group_channels[filename].addKey(timing-1, start_group)
+			if timing == 0 then
+				-- Handle 0ms bgsetlayer commands by removing the default 0ms background key
+				bg_layer_group_channels[filename].removeKeyAtTiming(0)
+			end
 			
 			if string.lower(layer_group) == "foreground" then
 				bg_layer_group_channels[filename].addKey(timing, "Foreground")
